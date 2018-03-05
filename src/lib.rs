04171fb553dc0a26b5003846e9c6c1ab053574d1
Wim Looman <wim.looman@kiwi.ki>
2018-03-05 13:26:47 +0100

    Support building in `no_std` environments

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,35 @@
+#![no_std]
+
 #![feature(arbitrary_self_types)]
 
 extern crate pin_api;
 extern crate futures_core;
 extern crate futures_executor;
 
-mod executor;
-mod unsafe_pin;
+macro_rules! if_std {
+    ($($i:item)*) => ($(
+        #[cfg(feature = "std")]
+        $i
+    )*)
+}
 
-use pin_api::{PinMut, PinBox, Unpin};
+use pin_api::{PinMut, Unpin};
 use futures_core::{Future, Stream, Poll, task};
 
-pub use executor::{StableExecutor, block_on_stable};
-use unsafe_pin::UnsafePin;
+if_std! {
+    mod executor;
+    mod unsafe_pin;
 
-pub type PinnedFuture<'a, T, E> = PinBox<Future<Item = T, Error = E> + 'a>;
-pub type PinnedFutureSend<'a, T, E> = PinBox<Future<Item = T, Error = E> + Send + 'a>;
-pub type PinnedStream<'a, T, E> = PinBox<Stream<Item = T, Error = E> + 'a>;
-pub type PinnedStreamSend<'a, T, E> = PinBox<Stream<Item = T, Error = E> + Send + 'a>;
+    use pin_api::PinBox;
+
+    pub use executor::{StableExecutor, block_on_stable};
+    use unsafe_pin::UnsafePin;
+
+    pub type PinnedFuture<'a, T, E> = PinBox<Future<Item = T, Error = E> + 'a>;
+    pub type PinnedFutureSend<'a, T, E> = PinBox<Future<Item = T, Error = E> + Send + 'a>;
+    pub type PinnedStream<'a, T, E> = PinBox<Stream<Item = T, Error = E> + 'a>;
+    pub type PinnedStreamSend<'a, T, E> = PinBox<Stream<Item = T, Error = E> + Send + 'a>;
+}
 
 pub trait StableFuture {
     type Item;
@@ -24,12 +37,14 @@ pub trait StableFuture {
 
     fn poll(self: PinMut<Self>, ctx: &mut task::Context) -> Poll<Self::Item, Self::Error>;
 
+    #[cfg(feature = "std")]
     fn pin<'a>(self) -> PinnedFutureSend<'a, Self::Item, Self::Error>
         where Self: Send + Sized + 'a
     {
         PinBox::new(unsafe { UnsafePin::new(self) })
     }
 
+    #[cfg(feature = "std")]
     fn pin_local<'a>(self) -> PinnedFuture<'a, Self::Item, Self::Error>
         where Self: Sized + 'a
     {
@@ -52,12 +67,14 @@ pub trait StableStream {
 
     fn poll_next(self: PinMut<Self>, ctx: &mut task::Context) -> Poll<Option<Self::Item>, Self::Error>;
 
+    #[cfg(feature = "std")]
     fn pin<'a>(self) -> PinnedStreamSend<'a, Self::Item, Self::Error>
         where Self: Send + Sized + 'a
     {
         PinBox::new(unsafe { UnsafePin::new(self) })
     }
 
+    #[cfg(feature = "std")]
     fn pin_local<'a>(self) -> PinnedStream<'a, Self::Item, Self::Error>
         where Self: Sized + 'a
     {
